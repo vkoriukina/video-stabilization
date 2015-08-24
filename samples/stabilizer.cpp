667cedb63c76b519b9977d4ab491951ef707669f
xamcaa <ss2015@ws-2k-114-06n.ccam.cmc.unn.net>
2015-08-24 15:55:27 +0300

    Revert "start with plotting"
    This reverts commit 7d084cb07f6a3d33c66ebc67ead44be90b1f4f73.

--- samples/stabilizer.cpp
+++ samples/stabilizer.cpp
@@ -13,12 +13,5 @@ bool Stabilizer::track( const cv::Mat& frame)
 
 bool generateFinalShift()
 {
-    cv::Mat image;
-    int radius=5;
-    for (int i=0; i<xshift.size(); i++)
-    {
-        image.at<Point2f>(
-        cv::circle(image,cvPoint(mypoints[i].x,mypoints[i].y),radius,CV_RGB(100,0,0),-1,8,0);
-    }
     return true;
 }
